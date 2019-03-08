864d5a9c46ecc89fd14a04bd29638a5b34b8d136
ShroukSaied <45403578+ShroukSaied@users.noreply.github.com>
2019-03-09 00:24:47 +0200

    Add files via upload

---
+++ WritableGUI.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package peertopeer.networking;
+
+/**
+ *
+ * @author MOHAMED
+ */
+public interface WritableGUI {
+    
+    void write(String s);
+}
