b4ab4e8c043d5184276de1cff0f206b29fe43b49
AyaAhmedEliwa <45417911+AyaAhmedEliwa@users.noreply.github.com>
2019-03-09 14:52:59 +0200

    Add files via upload

---
+++ PeerToPeer.java
@@ -0,0 +1,16 @@
+
+package peertopeer;
+
+import peertopeer.gui.MainScreen;
+
+
+public class PeerToPeer {
+
+    public static void main(String[] args) {
+        
+        MainScreen screen = new MainScreen();
+        screen.show();
+    
+    }
+    
+}
